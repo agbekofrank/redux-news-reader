93628db12c4bad155d157060f2085289f7150964
agbekofrank <frank.agbeko@amalitech.org>
2021-10-06 09:40:55 +0000

    Comment slice and comment js

--- src/features/comments/commentsSlice.js
+++ src/features/comments/commentsSlice.js
@@ -1,21 +1,47 @@
-import { createSlice } from '@reduxjs/toolkit';
 // Import createAsyncThunk and createSlice here.
-
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";  
 // Create loadCommentsForArticleId here.
+export const loadCommentsForArticleId = createAsyncThunk("comments/loadCommentsForArticleId", 
+  async (id) => {
+    const response = await fetch(`api/articles/${id}/comments`);
+    const json = await response.json(); 
+    debugger
+    return json; 
+  }
+)
 
 // Create postCommentForArticleId here.
 
 export const commentsSlice = createSlice({
-    name: 'comments',
-    initialState: {
-      // Add initial state properties here.
-    },
-    // Add extraReducers here.
-  });
-  
-  export const selectComments = (state) => state.comments.byArticleId;
-  export const isLoadingComments = (state) => state.comments.isLoadingComments;
-  export const createCommentIsPending = (state) => state.comments.createCommentIsPending;
-  
-  export default commentsSlice.reducer;
-  
+  name: 'comments',
+  initialState: {
+    // Add initial state properties here.
+    byArticleId: {},
+    isLoadingComments: false,
+    failedToLoadComments: false
+  },
+  // Add extraReducers here.
+  extraReducers: (builder) => {
+    builder
+      .addCase(loadCommentsForArticleId.pending, (state) => {
+        state.isLoadingComments = true;
+        state.failedToLoadComments = false;
+      })
+      .addCase(loadCommentsForArticleId.fulfilled, (state, action) => {
+        state.isLoadingComments = false;
+        state.failedToLoadComments = false;
+        state.byArticleId[action.payload.articleId] = action.payload.comments;
+      })
+      .addCase(loadCommentsForArticleId.rejected, (state) => {
+        state.isLoadingComments = false;
+        state.failedToLoadComments = true;
+        state.byArticleId = {};
+      })
+  }
+});
+
+export const selectComments = (state) => state.comments.byArticleId;
+export const isLoadingComments = (state) => state.comments.isLoadingComments;
+export const createCommentIsPending = (state) => state.comments.createCommentIsPending;
+
+export default commentsSlice.reducer;
